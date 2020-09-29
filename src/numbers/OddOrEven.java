06f5f49efb4b99790ada8843f5bade8c15591410
Ana <andreiana1995@gmail.com>
2020-09-29 10:21:28 +0300

    updated method for odd/even numbers

--- src/numbers/OddOrEven.java
+++ src/numbers/OddOrEven.java
@@ -5,29 +5,30 @@ import java.util.Scanner;
 
 public class OddOrEven {
 
-	void isOddOrEven(int a) {
+	public boolean isOddOrEven(int a) {
+		/*
+		 * return true if condition is true;
+		 * return (a%2==0);
+		 * */
 
 		if (a % 2 == 0)
-			System.out.println("Number is even");
-
-		else
-			System.out.println("number is odd");
+			return true;
+		// return false is executed only if first condition is not true; 
+			return false;
 
 	}
 
 	public static void main(String[] args) {
-		try (Scanner input = new Scanner(System.in)) {
-			System.out.println("Input the number: ");
-			int a = input.nextInt();
-			OddOrEven o = new OddOrEven();
-			o.isOddOrEven(a);
-		}catch(InputMismatchException e ) {
-			System.out.println("Input should be a number");
-		}
-		
-		//if don't use the user's input
-		/*OddOrEven o = new OddOrEven();
-		o.isOddOrEven(10);*/
+		/*
+		 * try (Scanner input = new Scanner(System.in)) {
+		 * System.out.println("Input the number: "); int a = input.nextInt(); OddOrEven
+		 * o = new OddOrEven(); o.isOddOrEven(a); }catch(InputMismatchException e ) {
+		 * System.out.println("Input should be a number"); }
+		 */
+
+		// if don't use the user's input
+		OddOrEven o = new OddOrEven();
+		System.out.println(o.isOddOrEven(10));
 
 	}
 
